a214b05c82fd2bf299cfa210f7cad55e322a6767
Kazi Lotus <kazilotus@hotmail.com>
2018-10-29 23:37:37 +0600

    admin page for viewing applicants

--- resources/views/econprodigy/admin.blade.php
+++ resources/views/econprodigy/admin.blade.php
@@ -17,16 +17,40 @@
     <div class="flex-center position-ref">
         
         <div id="fullpage" class="content w-100">
-            
-            @foreach ($teams as $team)
 
-                @foreach ($team->applicants as $applicant)
+            <table class="table">
+                <thead class="thead-light">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Team Name</th>
+                        <th scope="col">University Name</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">University ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($teams as $team)
 
-                    {{ dd($applicant->name) }} 
+                        @foreach ($team->applicants as $applicant)
 
-                @endforeach
+                            <tr>
+                                <th scope="row">{{ $team->id }}</th>
+                                <td>{{ $team->team_name }}</td>
+                                <td>{{ $team->university_name }}</td>
+                                <td>{{ $applicant->name }}</td>
+                                <td>{{ $applicant->phone_no }}</td>
+                                <td>{{ $applicant->email }}</td>
+                                <td>{{ $applicant->uni_id }}</td>
+                            </tr>
 
-            @endforeach
+                        @endforeach
+
+                    @endforeach
+
+                </tbody>
+            </table>
 
         </div>
 
